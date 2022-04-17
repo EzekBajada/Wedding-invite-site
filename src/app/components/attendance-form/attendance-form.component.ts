12a8dd2cd8e71688cbd92d80bdf323ec5c04b1b3
Ezekiel Bajada <ezekielbajada@thynksoftware.com>
2022-04-17 18:14:29 +0200

    Added surname validation

--- src/app/components/attendance-form/attendance-form.component.ts
+++ src/app/components/attendance-form/attendance-form.component.ts
@@ -1,8 +1,15 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {GuestService} from '../../services/guest.service';
-import {NotificationService} from "../../services/notification.service";
-import {NotificationType} from "../../models/NotificationMessage";
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { GuestService } from '../../services/guest.service';
+import { NotificationService } from '../../services/notification.service';
+import { NotificationType } from '../../models/NotificationMessage';
 
 @Component({
   selector: 'app-attendance-form',
@@ -10,7 +17,10 @@ import {NotificationType} from "../../models/NotificationMessage";
 })
 export class AttendanceFormComponent implements OnInit {
   form = new FormGroup({
-    name: new FormControl('', [Validators.required]),
+    name: new FormControl('', [
+      Validators.required,
+      this.surnameValidator(/\s[a-z]+/),
+    ]),
     numberOfGuests: new FormControl('', [
       Validators.required,
       Validators.min(1),
@@ -26,13 +36,12 @@ export class AttendanceFormComponent implements OnInit {
 
   isAttending: boolean = false;
 
-  constructor(private guestService: GuestService, private notificationService: NotificationService) {}
+  constructor(
+    private guestService: GuestService,
+    private notificationService: NotificationService
+  ) {}
 
-  ngOnInit(): void {
-    this.guestService.getAllGuests().subscribe(x=> {
-        console.log(x);
-    })
-  }
+  ngOnInit(): void {}
 
   toggleGuestForm(attendance: boolean) {
     this.isAttending = attendance;
@@ -50,22 +59,47 @@ export class AttendanceFormComponent implements OnInit {
     this.form.markAllAsTouched();
 
     if (this.form.valid || (this.name?.valid && !this.isAttending)) {
-      this.guestService
-        .findGuest(this.name?.value)
-        .subscribe((x) => {});
+      this.guestService.findGuest(this.name?.value).subscribe((x) => {});
       this.formSubmittedSuccessfully.emit(this.isAttending);
       this.inviteName.emit(this.name?.value);
       this.form.reset();
     } else {
       this.notificationService.sendMessage({
-        message: "Check form again before submitting!",
-        type: NotificationType.error
-      })
+        message: 'Check form again before submitting!',
+        type: NotificationType.error,
+      });
     }
   }
 
   onCancel() {
     this.formSubmittedSuccessfully.emit(false);
     this.form.reset();
+  }
+
+  surnameValidator(regExp: RegExp): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const isThereWordAfterSpace = regExp.test(control.value);
+      return !isThereWordAfterSpace
+        ? { noSurname: { value: control.value } }
+        : null;
+    };
+  }
+
+  get isRequiredError() {
+    return (
+      this.name?.invalid &&
+      this.name?.errors &&
+      this.name?.errors['required'] &&
+      (this.name?.dirty || this.name?.touched)
+    );
+  }
+
+  get noSurnameError() {
+    return (
+      this.name?.invalid &&
+      this.name?.errors &&
+      this.name?.errors['noSurname'] &&
+      (this.name?.dirty || this.name?.touched)
+    );
   }
 }
